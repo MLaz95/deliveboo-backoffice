f2c9b069f8d290e8994ff933e390d9ec39b9698b
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-05-16 11:55:09 +0200

    Fixed form user+restaurant

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,6 +14,8 @@
                         @csrf
 
                         <div class="mb-4 row">
+                            <h3 class="mb-4">User data</h3>
+                            
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
@@ -91,13 +93,86 @@
                             </div>
                         </div>
 
+                       
+                        <h3 class="mb-4">Restaurant data</h3>
+                        
+
+                        <div class="mb-4 row">
+                            <label for="name_res" class="col-md-4 col-form-label text-md-right">{{ __('Restaurant Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name_res" type="text" class="form-control @error('name_res') is-invalid @enderror" name="name_res" value="{{ old('name_res') }}" required maxlength="100" autocomplete="name_res" autofocus>
+
+                                @error('name_res')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="mb-4 row">
+                            <label for="address_res" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="address_res" type="text" class="form-control @error('address_res') is-invalid @enderror" name="address_res" value="{{ old('address_res') }}" required maxlength="100" autocomplete="address_res" autofocus>
+
+                                @error('address_res')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-4 row">
+                            <label for="img_res" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="img_res" type="file" class="form-control @error('img_res') is-invalid @enderror" name="img_res" value="{{ old('img_res') }}" required accept=".jpg, .bpm, .png, .svg" autocomplete="img_res" autofocus>
+
+                                @error('img_res')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-4 row">
+                            <label for="img_res" class="col-md-4 col-form-label text-md-right">{{ __('Category') }}</label>
+
+                            <div class="col-md-6">
+                                <div class="d-flex gap-3 flex-wrap ">
+                                    @foreach($categories as $category)
+                                    <div class="form-check">
+                                        <input 
+                                            type="checkbox" 
+                                            name="categories[]" 
+                                            value="{{$category->id}}" 
+                                            class="form-check-input" 
+                                            id="category-{{$category->id}}"
+                                            {{in_array($category->id, old('categories', [])) ? 'checked' : ''}}
+                                        >
+                                        <label 
+                                            class="form-check-label" 
+                                            for="category-{{$category->id}}"
+                                        >{{$category->name}}</label>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Next') }}
+                                    {{ __('Submit') }}
                                 </button>
                             </div>
                         </div>
+
                     </form>
                 </div>
             </div>
