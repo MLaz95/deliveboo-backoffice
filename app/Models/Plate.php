3b613f4f74572efb956dd02f7dbf0620854406c5
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-16 16:10:31 +0200

    create table orders and table orders-plates

--- app/Models/Plate.php
+++ app/Models/Plate.php
@@ -19,4 +19,8 @@ class Plate extends Model
     public function restaurant(){
         return $this->belongsTo(Restaurant::class);
     }
+
+    public function orders(){
+        return $this->belongsToMany(Order::class);
+    }
 }
