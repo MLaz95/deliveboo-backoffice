e226a2305e229e6089fb70c7f07c1335ff26a91f
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-05-17 10:49:45 +0200

    creata funzione filter nel category controller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -14,6 +16,19 @@ class CategoryController extends Controller
         return response()->json([
             "success" => true,
             "results" => $categories,
+        ]);
+    }
+
+    public function filter($id){
+        $filtered = DB::table('restaurants')
+                        ->join('category_restaurant', 'restaurants.id', '=', 'category_restaurant.restaurant_id')
+                        ->select('*')
+                        ->where('category_restaurant.category_id', $id)
+                        ->get();
+
+        return response()->json([
+            "success" => true,
+            "results" => $filtered,
         ]);
     }
 }
