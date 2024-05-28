4aa802ad5cc1134a3c652bc2b8586efc4ac13edf
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-28 11:41:29 +0200

    first email test

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewOrder;
+use App\Models\Lead;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
@@ -28,8 +32,31 @@ class OrderController extends Controller
             $newOrder->plates()->attach($plate['id'], ['quantity' => $plate['quantity']]);
         }
 
+        // email
+        // $validator = Validator::make($request->all(), [
+        //     'name' => 'required',
+        //     'address' => 'required|email',
+        //     'message' => 'required'
+        // ], [
+        //     'name.required' => "Devi inserire il tuo nome",
+        //     'address.required' => "Devi inserire la tua email",
+        //     'address.email' => "Devi inserire una mail corretta",
+        //     'message.required' => "Devi inserire un messaggio",
+        // ]);
+
+        //salvataggio del db
+        $newLead = new Lead();
+        $newLead->fill($request->all());
+        $newLead->save();
+
+        //invio mail
+        Mail::to('mikypupo@hotmail.it')->send(new NewOrder($newLead));
+
+        // Risposta al client
         return response()->json([
-            "success" => true,
+            'success' => true,
+            'message' => 'Richiesta di contatto inviato correttamente',
+            'request' => $request->all(),
             "results" => $newOrder,
         ]);
     }
---
+++ resources/views/emails/new-order.blade.php
@@ -0,0 +1,18 @@
+<h1>
+    Deliveboo: Nuovo ordine
+</h1>
+
+<p>
+    <ul>
+        <li>
+            Da: {{$lead->name}}
+        </li>
+        <li>
+            Email: {{$lead->address}}
+        </li>
+        <li>
+            Messaggio: <br>
+            {{$lead->message}}
+        </li>
+    </ul>
+</p>
