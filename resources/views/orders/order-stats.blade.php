3f8f3fb9005bce09db0fc81fcc65aa699e3bb10b
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-29 22:09:53 +0200

    add button back

--- resources/views/orders/order-stats.blade.php
+++ resources/views/orders/order-stats.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 
-<div style="width: 75%; background-color: white;">
+<div style="width: 50%; background-color: white;">
     {!! $chartjs->render() !!}
+</div>
+<div class="text-center mt-5 mx-5">
+    <a href="{{ route('restaurant') }}" class="btn btn-secondary">Back to Restaurant</a>
 </div>
 
 {{-- @dd($totals) --}}
