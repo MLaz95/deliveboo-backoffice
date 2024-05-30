62b8c98346f49c7e6aadd2a50efb1ab48dbf5a67
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-30 15:37:19 +0200

    create OrderSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -21,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RestaurantSeeder::class,
             CategorySeeder::class,
+            OrderSeeder::class
         ]);
     }
 }
