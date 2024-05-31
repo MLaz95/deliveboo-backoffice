5fcf5e1e6c99b45819d7cf1dc686537833f272e3
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-05-31 16:53:39 +0200

    fix button

--- resources/views/plates/index.blade.php
+++ resources/views/plates/index.blade.php
@@ -4,20 +4,24 @@
 <div class="index-box">
     
     <div class="container">
-        <div class="my-4 d-flex flex-column flex-lg-row justify-content-between align-items-center">
-            <div class="mb-2 mb-lg-0">
-                <a class="btn btn-primary text-decoration-none fs-3" href="{{ route('restaurant') }}"><i class="fa-solid fa-left-long"></i></a>
-            </div>
-            <div>
-                <a class="btn btn-success text-decoration-none fs-3" href="{{ route('plates.create') }}"><i class="fa-solid fa-plus"></i></a>
-            </div>
-        </div>
+       
         <div class="row justify-content-center">
             <div class="col-12">
                 <div class="card p-3">
-                    <div class=" fw-bolder text-center">
-                        <h1 class="fw-bold">Your Plates</h1>
+                    <div class="my-4 d-flex flex-column flex-lg-row justify-content-between align-items-center">
+                        <div class="mb-2 mb-lg-0">
+                            <a class="btn btn-primary text-decoration-none fs-3" href="{{ route('restaurant') }}"><i class="fa-solid fa-left-long"></i></a>
+                        </div>
+
+                        <div class=" fw-bolder text-center">
+                            <h1 class="fw-bold">Your Plates</h1>
+                        </div>
+
+                        <div>
+                            <a class="btn btn-success text-decoration-none fs-3" href="{{ route('plates.create') }}"><i class="fa-solid fa-plus"></i></a>
+                        </div>
                     </div>
+                    
                     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
                         @foreach ($plates as $plate)
                         <div class="col">
