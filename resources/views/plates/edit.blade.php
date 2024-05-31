bd6646aa7c0abcad991e1643456d2c7a705afd9f
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-31 16:24:02 +0200

    style edit page

--- resources/views/plates/edit.blade.php
+++ resources/views/plates/edit.blade.php
@@ -5,10 +5,10 @@
 
     <div class="container pb-5 ">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <h1>Edit</h1>
+            <div class="col-md-8" style="background-color: #f3d9bf; padding: 20px; box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;">
+                <div>
+                    <div>
+                        <h1>Edit plate</h1>
                     </div>
     
                     <div class="card-body">
@@ -85,9 +85,9 @@
                             </div>
     
                             
-                                <div class="d-flex gap-2 justify-content-center">
-                                    <a class="btn btn-secondary text-decoration-none " href="{{route('plates.show', $plate->id)}}">Back</a>
-                                    <button type="submit" class="btn btn-primary">
+                                <div class="d-flex gap-5 justify-content-center">
+                                    <a class="btn text-decoration-none text-white" style="background-color: #d62300" href="{{route('plates.show', $plate->id)}}">Back</a>
+                                    <button type="submit" class="btn btn-primary text-white border-0" style="background-color: #279647">
                                         Update
                                     </button>
                                 </div>
