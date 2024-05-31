c8ba18bfc879623afc09fe141453deb145d529b3
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-05-31 16:37:04 +0200

    responsive index

--- resources/views/plates/index.blade.php
+++ resources/views/plates/index.blade.php
@@ -4,33 +4,33 @@
 <div class="index-box">
     
     <div class="container">
-        <div class="my-4 d-flex justify-content-between align-items-center">
-            <div>
-                <a class="btn btn-primary text-decoration-none " href="{{ route('restaurant') }}">Back</a>
+        <div class="my-4 d-flex flex-column flex-lg-row justify-content-between align-items-center">
+            <div class="mb-2 mb-lg-0">
+                <a class="btn btn-primary text-decoration-none" href="{{ route('restaurant') }}">Back</a>
             </div>
             <div>
-                <a class="btn btn-primary text-decoration-none " href="{{ route('plates.create') }}">Create Plate</a>
+                <a class="btn btn-primary text-decoration-none" href="{{ route('plates.create') }}">Create Plate</a>
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header fw-bolder text-center">
-                        <h1 class=" fw-bold ">Your Plates</h1>
+            <div class="col-12">
+                <div class="card p-3">
+                    <div class=" fw-bolder text-center">
+                        <h1 class="fw-bold">Your Plates</h1>
                     </div>
-                    <div class="row row-cols-1 row-cols-lg-4">
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
                         @foreach ($plates as $plate)
-                        <div class="col rounded-4">
+                        <div class="col">
                             <a href="{{ route('plates.show', $plate->id) }}" class="text-decoration-none">
-                                <div class="card m-2 border-1 rounded-5 shadow-lg" style="width: 18rem;">
-                                    <img src="{{ asset('storage/' . $plate->image) }}" class="card-img-top object-fit-cover"
-                                        alt="{{ $plate->name }}" style="height: 250px;">
+                                <div class="card m-2 border-1 rounded-5 shadow-lg">
+                                    <img src="{{ asset('storage/' . $plate->image) }}" class="card-img-top object-fit-cover" alt="{{ $plate->name }}" style="height: 250px;">
                                     <div class="card-body">
                                         <h5 class="card-title">{{ $plate->name }}</h5>
                                         <div class="d-flex justify-content-between">
                                             <h6 class="card-text">{{ $plate->price }} &euro;</h6>
                                             @if($plate->visible)
                                             <p>Visible</p>
+                                            <i class="fa-light fa-location-dot"></i>
                                             @else
                                             <p>Not Visible</p>
                                             @endif
@@ -41,12 +41,9 @@
                         </div>
                         @endforeach
                     </div>
-    
                 </div>
             </div>
         </div>
     </div>
-
-</div>
 
 @endsection
