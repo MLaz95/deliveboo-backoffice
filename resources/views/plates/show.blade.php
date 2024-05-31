d2b975c36234f3c640d0c7ce19ef453074587bbd
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-31 16:18:17 +0200

    style create page

--- resources/views/plates/show.blade.php
+++ resources/views/plates/show.blade.php
@@ -6,7 +6,7 @@
     <div class="container d-flex flex-column align-items-center">
       
         <div class="row justify-content-center">
-            <div class="col 6 rounded 4" style="background-color: #f3d9bf; padding: 20px; box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;"">
+            <div class="col 6 rounded 4" style="background-color: #f3d9bf; padding: 20px; box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;">
                 <div class="d-flex flex-column justify-content-center gap-3">
                   <div>
                     <img src="{{ asset('storage/' . $plate->image) }}" class="img-fluid w-100" alt="@" style="max-height: 400px">
