e5c7440faa6e381beb0dc06c800d4d81207bb9ea
michele-fabio-pupo <mikypupo@hotmail.it>
2024-06-01 08:33:04 +0200

    fix card dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,18 +24,18 @@
                             <img class="rounded-5 p-3 img-fluid" src="{{ asset('storage/' . $restaurant->img_res) }}" alt="{{ $restaurant->name }}" style="width: 100%;">
                         </div>
                         
-                        <div class="d-flex flex-column gap-3">
+                        <div class="d-flex flex-column gap-3 pt-4 align-items-center">
                             <h3 class="adress text-start text-black">
                                 <i class="fa-solid fa-location-dot p-1"></i> {{$restaurant->address_res}}
                             </h3>
-                            <div class="d-flex gap-2 align-items-center fw-bolder p-2 overflow-x-auto">
+                            <div class="d-flex gap-2 align-items-center fw-bolder p-2 overflow-x-auto flex-wrap">
                                 <div class=" vanish">Categories:</div>
                                 @foreach($restaurant->categories as $category)
                                 <span class="categories badge rounded-pill rounded rounded-3 text-black">{{ $category->name }}</span>
                                 @endforeach
                             </div>
     
-                            <div class="d-flex align-items-start gap-4 pt-4 ps-5">
+                            <div class="d-flex justify-content-center gap-4 pt-4 me-lg-3">
                                 <a href="{{route('plates.index')}}" class="btn text-decoration-none text-white" style="background-color: #e67e22">Menu</a>
                                 <a href="{{ route('order-summary') }}" class="btn text-white" style="background-color: #279647">Orders</a>
                             </div>
